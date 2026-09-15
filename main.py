e71f2bc5a1f5de87636169b666e86e46ae7905c3
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-09-15 19:52:06 -0400

    Add initial Streamlit setup for Access Agent application

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from access_agent_scout.graph import compiled_graph
+from access_agent_scout.state import AssessmentState
 import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()
+
+
+st.set_page_config("Access Agent", page_icon="♿")
+st.title("Access Agent")
+st.write("hello this is my first time using streamlit")
